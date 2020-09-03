d72b3f8dc8167be32c25e0d383178faf2b54fdfc
Ankit Shrivastava <ankit_shrivastava@persistent.com>
2020-09-03 11:12:18 +0530

    updating for region error.

--- mlops.py
+++ mlops.py
@@ -7,32 +7,6 @@ import boto3
 import mlflow
 import mlflow.sagemaker as mfs
 
-def create_repository(repository_name="mlflow-pyfunc"):    
-    client = boto3.session.Session().client('ecr')
-    try:
-        response = client.describe_repositories(repositoryNames=[repository_name])
-        print(f"Repository '{repository_name}' already exist.")
-    except client.exceptions.RepositoryNotFoundException:
-        print(f"Repository '{repository_name}' not found, will create a new named repository")
-        try:
-            tags = [
-                { 
-                    "Key": "Name",
-                    "Value": repository_name
-                },
-                {
-                    "Key": "Description",
-                    "Value": "MlFlow automation repo"
-                }
-            ]
-            encryptionConfiguration = { "encryptionType": "AES256" }
-            response = client.create_repository(repositoryName=repository_name, tags=tags, encryptionConfiguration=encryptionConfiguration)
-            print(f"Repository '{repository_name}' created")
-        except Exception as e:
-            raise e
-    except Exception as e:
-        raise e
-
 
 def get_account_id():
     try:
@@ -41,7 +15,7 @@ def get_account_id():
         account_id = caller_id['Account']
         return account_id
     except Exception as e:
-        print ("Failed to get account id from assume role")
+        print("Failed to get account id from assume role")
         return None
 
 
@@ -51,8 +25,38 @@ def get_region():
         region = session.region_name or 'us-east-1'
         return region
     except Exception as e:
-        print ("Failed to get current region from server")
+        print("Failed to get current region from server")
         return None
+
+
+def create_repository(repository_name="mlflow-pyfunc", region_name=get_region()):
+    client = boto3.session.Session().client('ecr', region_name=region_name)
+    try:
+        response = client.describe_repositories(
+            repositoryNames=[repository_name])
+        print(f"Repository '{repository_name}' already exist.")
+    except client.exceptions.RepositoryNotFoundException:
+        print(
+            f"Repository '{repository_name}' not found, will create a new named repository")
+        try:
+            tags = [
+                {
+                    "Key": "Name",
+                    "Value": repository_name
+                },
+                {
+                    "Key": "Description",
+                    "Value": "MlFlow automation repo"
+                }
+            ]
+            encryptionConfiguration = {"encryptionType": "AES256"}
+            response = client.create_repository(
+                repositoryName=repository_name, tags=tags, encryptionConfiguration=encryptionConfiguration)
+            print(f"Repository '{repository_name}' created")
+        except Exception as e:
+            raise e
+    except Exception as e:
+        raise e
 
 
 def get_ecr_url(image_name="mlflow-pyfunc", region=get_region()):
@@ -70,7 +74,8 @@ def get_ecr_url(image_name="mlflow-pyfunc", region=get_region()):
 def check_sagemaker_endpoint_status(app_name, region=get_region()):
     try:
         sage_client = boto3.session.Session().client('sagemaker', region_name=region)
-        endpoint_description = sage_client.describe_endpoint(EndpointName=app_name)
+        endpoint_description = sage_client.describe_endpoint(
+            EndpointName=app_name)
         endpoint_status = endpoint_description["EndpointStatus"]
         return endpoint_status
     except Exception as e:
@@ -86,7 +91,7 @@ def execute_shell(command, wait=True, current_env=True, working_dir=None):
     stdout = None
     if current_env:
         env = copy.deepcopy(os.environ)
-    
+
     process = subprocess.Popen(
         command, shell=True, stdout=stdout, env=env, cwd=working_dir)
     if wait:
@@ -94,7 +99,7 @@ def execute_shell(command, wait=True, current_env=True, working_dir=None):
 
 
 def create_mlflow_base_docker_image(image_name="mlflow-pyfunc"):
-    command = "mlflow sagemaker build-and-push-container --no-push"
+    command = f"mlflow sagemaker build-and-push-container --no-push --container {image_name}"
     execute_shell(command=command)
 
 
@@ -118,7 +123,7 @@ def deploy_model(
     sagemaker_instance_type=mfs.DEFAULT_SAGEMAKER_INSTANCE_TYPE,
     sagemaker_instance_count=mfs.DEFAULT_SAGEMAKER_INSTANCE_COUNT,
     sagemaker_model_mode=mfs.DEPLOYMENT_MODE_CREATE,
-    artifact_root = None
+    artifact_root=None
 ):
     # Verify Model before deploy
     status = check_sagemaker_endpoint_status(app_name=app_name)
@@ -126,15 +131,16 @@ def deploy_model(
         msg = f"AWS Sagemaker endpoint {app_name} exist."
         print(msg)
         raise ValueError(msg)
-    
+
     if not artifact_root:
         artifact_root = f"s3://{bucket}/prod/mlflow"
-    
-    image_ecr_url = get_ecr_url(region=region)    
+
+    image_ecr_url = get_ecr_url(region=region)
     execution_role_arn = get_sagemager_execution_roler_arn(execution_role_name)
     model_uri = f"{artifact_root}/{experiment_id}/{run_id}/artifacts/model"
-    
-    mfs.deploy(app_name=app_name, execution_role_arn=execution_role_arn, bucket=bucket, model_uri=model_uri, image_url=image_ecr_url, region_name=region, mode=sagemaker_model_mode, instance_type=sagemaker_instance_type, instance_count=sagemaker_instance_count)
+
+    mfs.deploy(app_name=app_name, execution_role_arn=execution_role_arn, bucket=bucket, model_uri=model_uri, image_url=image_ecr_url,
+               region_name=region, mode=sagemaker_model_mode, instance_type=sagemaker_instance_type, instance_count=sagemaker_instance_count)
 
 
 def destroy_model(app_name, region=get_region()):
@@ -177,30 +183,31 @@ def get_json_data(file_name):
         print(f"Input file does not exists : {file_name}")
     return None
 
+
 def push_image_to_sagemaker(
     repository_name="mlflow-pyfunc",
     image_name="mlflow-pyfunc"
 ):
     # Create or verify the repository in AWS ECR
     create_repository(repository_name=repository_name)
-    
+
     # Create base docker image for AWS Sagemaker
     create_mlflow_base_docker_image(image_name=image_name)
-    
+
     # Push base docker image to AWS ECR
     push_mlflow_base_docker_image(image_name=image_name)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # Create or verify the repository in AWS ECR
     push_image_to_sagemaker()
-    
+
     app_name = os.environ['APP_NAME']
     experiment_id = os.environ['EXPERIMENT_ID']
     run_id = os.environ['RUN_ID']
     bucket = os.environ['BUCKET']
     input_test_file = os.environ['INPUT_TEST_FILE']
-    
+
     # Verify Model before deploy
     status = check_sagemaker_endpoint_status(app_name=app_name)
     if status:
@@ -214,7 +221,7 @@ if __name__=="__main__":
         experiment_id=experiment_id,
         run_id=run_id,
         bucket=bucket)
-    
+
     # Verify Model status
     status = str(check_sagemaker_endpoint_status(app_name=app_name))
     print(f"Model {app_name} Endpoint status is '{status}'")
@@ -223,7 +230,8 @@ if __name__=="__main__":
         # Infer model here
         input_json = get_json_data(input_test_file)
         if input_json:
-            prediction = inferance_sagemaker_endpoint(app_name=app_name, input_json=input_json)
+            prediction = inferance_sagemaker_endpoint(
+                app_name=app_name, input_json=input_json)
             print(f"Received prediction response: {prediction}")
 
     # Delete model
